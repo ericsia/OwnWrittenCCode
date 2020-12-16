8cf1c0f074c0cba5d485898dd47efd2bcca4c498
ericsia <ericsia12@gmail.com>
2020-12-17 03:43:04 +0800

    Add files via upload

--- eric.c
+++ eric.c
@@ -15,3 +15,27 @@ int getInt(char *msg, int min, int max)
 	
 	return result;
 }
+
+/*remember to free(variable); to reclaim memory*/
+char* getText(char* msg)
+{
+	/*According the ISO IEC 7813 the cardholder name length must be 2 to 26 characters 
+	including first name, last name and spaces*/
+	char result[26];
+	int size;
+
+	do
+	{
+		printf(msg);
+		fgets(result, 26, stdin);
+		/*fgets() small input last character in the array will be '\n'*/
+		size = strlen(result);
+		result[size - 1] = '\0';
+	} while (result[0] == ' ' || result[0] == NULL);
+
+	/*The array char result[26] is local to getText(char* msg). 
+	After returns, that memory is reclaimed and can be used by other code.
+	To fix that you shall not return pointers to local variables, 
+	instead use a dynamically allocated copy of the string*/
+	return strdup(result);
+}
